caf909057ae6324fc7f33bb926fead3e0345a7f1
leeyelan <yr00868@sookmyung.ac.kr>
2020-11-05 16:30:44 +0900

    6th

--- main.c
+++ main.c
@@ -3,27 +3,36 @@
 
 
 	
-	void swap(int *x, int *y) {
-		
-        int temp;
-        temp = *x;
-        *x = *y;
-        *y = temp;
-		
+int main(void){
 	
-		
-		
-		
-	}
-	
-	void main(void){
-		
-		int a=3;
-        int b = 5;
-		swap(&a,&b);
-		printf("a: %i, b: %i\n",a,b);	
+	int i;
+	int grade[5];
 
-	}
+	int sum = 0;
+	
+	int *ptr;
 
 	
 
+	
+	
+	for(i=0; i<5; i++)
+	{
+		ptr = &grade[i];
+		printf("grade[%i]=", i);
+		scanf("%d", ptr);
+		}	
+	
+	ptr = grade;
+	
+ 	for(i=0; i<5; i++)
+	{
+		ptr = &grade[i];
+		sum += *ptr;
+		printf("grade[%d]= %d\n",i, *ptr);
+       }
+
+	printf("average = %i\n", sum/5);
+	
+	return 0;
+}
